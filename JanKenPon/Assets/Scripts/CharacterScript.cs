d2f4dcb69679f8845b228b7ded84b70220bc64fa
Burnsychocoes <32386624+Burnsychocoes@users.noreply.github.com>
2018-04-14 21:27:53 +0200

    firstScripts

---
+++ JanKenPon/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+    //[Serialized Field]
+    protected Weapon weapon;
+
+    //[Serialized Field]
+    protected int maxHp;
+    protected int hp;
+
+    //[Serialized Field]
+    protected float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+        hp = maxHp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected void Move()
+    {
+        if (!weapon.inRangeForAttack())
+        {
+
+        }
+    }
+}
---
+++ JanKenPon/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    protected Collider collider;
+
+    //[Serialized Field]
+    protected int damage;
+
+    //attack speed in hit/sec
+    //[Serialized Field]
+    protected float attackSpeed;
+
+    //[Serialized Field]
+    protected float range;
+
+    //cd for the next attack
+    protected float cd;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool inRangeForAttack()
+    {
+        return false;
+    }
+}
